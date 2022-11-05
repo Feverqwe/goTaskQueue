379b26c0a99b439f247f9fe2e185d896e33d7d48
Anton <leonardspbox@gmail.com>
2022-11-05 21:49:42 +0500

    ui dist

--- scripts/build.resources.sh
+++ scripts/build.resources.sh
@@ -6,6 +6,6 @@ cd $SCRIPT_DIR/..
 cd assets
 go get github.com/jteeuwen/go-bindata/...
 go install github.com/jteeuwen/go-bindata/...
-~/go/bin/go-bindata ./icon.ico ./www
+~/go/bin/go-bindata ./icon.ico ./www ./www/icons
 sed 's/package main/package assets/g' ./bindata.go > ./bindata_.go
 mv ./bindata_.go ./bindata.go
