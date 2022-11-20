78bac0f62356d30f0132d45d9fc4a7778f946d9c
Anton <leonardspbox@gmail.com>
2022-11-20 23:04:12 +0500

    fix

--- tq-ui/src/components/TaskList/components/TaskItem.tsx
+++ tq-ui/src/components/TaskList/components/TaskItem.tsx
@@ -1,5 +1,5 @@
 import {Box, Card, CardActionArea, IconButton} from '@mui/material';
-import React, {FC, SyntheticEvent, useCallback} from 'react';
+import React, {FC, SyntheticEvent, useCallback, useMemo} from 'react';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import ClearIcon from '@mui/icons-material/Clear';
 import StopIcon from '@mui/icons-material/Stop';
@@ -20,17 +20,17 @@ const TaskItem: FC<TaskItemProps> = ({task, onUpdate}) => {
   const {id, state} = task;
   const navigate = useNavigate();
 
-  const handleDelete = useCallback(async (e: SyntheticEvent) => {
+  const handleDelete = useCallback(async () => {
     await api.delete({id});
     onUpdate();
   }, [id, onUpdate]);
 
-  const handleStart = useCallback(async (e: SyntheticEvent) => {
+  const handleStart = useCallback(async () => {
     await api.taskRun({id});
     onUpdate();
   }, [id, onUpdate]);
 
-  const handleStop = useCallback(async (e: SyntheticEvent) => {
+  const handleStop = useCallback(async () => {
     await api.taskKill({id});
     onUpdate();
   }, [id, onUpdate]);
@@ -39,6 +39,43 @@ const TaskItem: FC<TaskItemProps> = ({task, onUpdate}) => {
     e.preventDefault();
     navigate(`task?id=${id}`);
   }, [navigate, id]);
+
+  const extraButtons = useMemo(() => {
+    const result = [];
+    if (task.links.length > 0) {
+      result.push(
+        <Box key={result.length} display="flex" pl={result.length ? 1 : 0}>
+          <TaskLinks task={task} />
+        </Box>,
+      );
+    }
+    if ([TaskState.Started, TaskState.Idle].includes(state)) {
+      result.push(
+        <Box key={result.length} display="flex" pl={result.length ? 1 : 0}>
+          {state === TaskState.Started && (
+            <IconButton onClick={handleStop} title="Stop">
+              <StopIcon />
+            </IconButton>
+          ) || state === TaskState.Idle && (
+            <IconButton onClick={handleStart} title="Start">
+              <PlayArrowIcon />
+            </IconButton>
+          )}
+        </Box>,
+      );
+    }
+    result.push(
+      <Box key={result.length} display="flex" pl={result.length ? 1 : 0} alignItems="center">
+        <TaskStatusIcon task={task} />
+      </Box>,
+    );
+    result.push(
+      <Box key={result.length} display="flex" pl={1} alignItems="center">
+        <KeyboardArrowRightIcon />
+      </Box>,
+    );
+    return result;
+  }, [handleStart, handleStop, state, task]);
 
   return (
     <Box px={1} pb={1}>
@@ -56,28 +93,7 @@ const TaskItem: FC<TaskItemProps> = ({task, onUpdate}) => {
               </Box>
             </CardActionArea>
           </Box>
-          {task.links.length > 0 && (
-          <Box display="flex">
-            <TaskLinks task={task} />
-          </Box>
-          )}
-          <Box display="flex" pl={task.links.length ? 1 : 0}>
-            {state === TaskState.Started && (
-            <IconButton onClick={handleStop} title="Stop">
-              <StopIcon />
-            </IconButton>
-            ) || state === TaskState.Idle && (
-            <IconButton onClick={handleStart} title="Start">
-              <PlayArrowIcon />
-            </IconButton>
-            )}
-          </Box>
-          <Box display="flex" pl={1} alignItems="center">
-            <TaskStatusIcon task={task} />
-          </Box>
-          <Box display="flex" pl={1} alignItems="center">
-            <KeyboardArrowRightIcon />
-          </Box>
+          {extraButtons}
         </Box>
       </Card>
     </Box>
