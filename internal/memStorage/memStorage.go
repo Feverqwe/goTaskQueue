5561b23d4c138d2a44cf517fc900cdb44af0388a
Anton <leonardspbox@gmail.com>
2023-01-28 10:14:05 +0500

    add storage mutex

--- internal/memStorage/memStorage.go
+++ internal/memStorage/memStorage.go
@@ -1,17 +1,22 @@
 package memstorage
 
+import "sync"
+
 type MemStorage struct {
 	keyValue map[string]interface{}
+	mrw      sync.RWMutex
 	ch       chan int
 }
 
 func (s *MemStorage) GetKeys(keys []string) map[string]interface{} {
 	result := make(map[string]interface{})
 	if keys == nil {
+		s.mrw.RLock()
 		result = s.keyValue
+		s.mrw.RUnlock()
 	} else {
 		for _, key := range keys {
-			if val, ok := s.keyValue[key]; ok {
+			if val, ok := s.GetKey(key); ok {
 				result[key] = val
 			}
 		}
@@ -19,17 +24,37 @@ func (s *MemStorage) GetKeys(keys []string) map[string]interface{} {
 	return result
 }
 
+func (s *MemStorage) GetKey(key string) (interface{}, bool) {
+	s.mrw.RLock()
+	defer s.mrw.RUnlock()
+	val, ok := s.keyValue[key]
+	return val, ok
+}
+
 func (s *MemStorage) SetObject(keyValue map[string]interface{}) error {
 	for key, value := range keyValue {
-		s.keyValue[key] = value
+		s.SetKey(key, value)
 	}
 	return nil
 }
+
+func (s *MemStorage) SetKey(key string, value interface{}) {
+	s.mrw.Lock()
+	defer s.mrw.Unlock()
+	s.keyValue[key] = value
+}
+
 func (s *MemStorage) DelKeys(keys []string) error {
 	for _, key := range keys {
-		delete(s.keyValue, key)
+		s.DelKey(key)
 	}
 	return nil
+}
+
+func (s *MemStorage) DelKey(key string) {
+	s.mrw.Lock()
+	defer s.mrw.Unlock()
+	delete(s.keyValue, key)
 }
 
 func GetMemStorage() *MemStorage {
