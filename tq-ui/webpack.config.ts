aebd7806f1774638e6d1b89181dad986fa1c3951
Anton <Feverqwe@users.noreply.github.com>
2023-05-03 22:20:53 +0500

    dep upd

--- tq-ui/webpack.config.ts
+++ tq-ui/webpack.config.ts
@@ -1,79 +1,83 @@
-import {Configuration} from "webpack";
-import * as HtmlWebpackPlugin from "html-webpack-plugin";
-import * as Path from "path";
-import * as CopyPlugin from "copy-webpack-plugin";
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import * as Path from 'path';
+import * as CopyPlugin from 'copy-webpack-plugin';
+import {CallableOption} from 'webpack-cli';
 
-export default (env: Record<string, any>, arg: Record<string, any>): Configuration => {
-  return {
-    entry: {
-      index: './src/index',
-    },
-    output: {
-      filename: '[name]-[contenthash].js',
-      chunkFilename: '[contenthash].chunk.js',
-      path: Path.resolve(__dirname, './dist'),
-    },
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            name: 'commons',
-            chunks: 'initial',
-            minChunks: 2,
-          },
+const getOptions: CallableOption = (env, argv) => ({
+  entry: {
+    index: './src/index',
+  },
+  output: {
+    filename: '[name]-[contenthash].js',
+    chunkFilename: '[contenthash].chunk.js',
+    path: Path.resolve(__dirname, './dist'),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: 'commons',
+          chunks: 'initial',
+          minChunks: 2,
         },
       },
     },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [{
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
             loader: 'ts-loader',
-          }],
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.png$/,
-          use: [{
-            loader: 'url-loader',
-          }],
-        },
-        {
-          test: /\.(less|css)$/,
-          use: [
-            {
-              loader: 'style-loader',
-            }, {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-              },
-            }, {
-              loader: 'less-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: './src/assets/index.html',
-        chunks: ['index'],
-        scriptLoading: 'blocking',
-      }),
-      new CopyPlugin({
-        patterns: [
-          { from: "./src/assets/icons", to: "icons" },
+          },
         ],
-      }),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
     ],
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-  };
-};
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/assets/index.html',
+      chunks: ['index'],
+      scriptLoading: 'blocking',
+    }),
+    new CopyPlugin({
+      patterns: [{from: './src/assets/icons', to: 'icons'}],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+});
+
+export default getOptions;
