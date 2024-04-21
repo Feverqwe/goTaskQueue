cc6dbc90c5dd07b13fc341a1c02a495cbb96f850
Anton <Feverqwe@users.noreply.github.com>
2024-04-21 17:48:55 +0500

    fix

--- tq-ui/src/utils/common.ts
+++ tq-ui/src/utils/common.ts
@@ -10,5 +10,5 @@ export function getDatetimeFormatter() {
 }
 
 export function isAbortError(err: unknown) {
-  return (err as Error).name !== 'AbortError';
+  return (err as Error).name === 'AbortError';
 }
